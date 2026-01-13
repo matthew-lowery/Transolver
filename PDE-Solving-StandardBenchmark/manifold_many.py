b00fd91a8c2e28db703cd71fcb0e90be9aa7007f
matthew-lowery <mattlowery10@gmail.com>
2026-01-12 18:35:04 -0700

    add transolver script for multiple manifolds

---
+++ PDE-Solving-StandardBenchmark/manifold_many.py
@@ -0,0 +1,239 @@
+import os
+import argparse
+import numpy as np
+import torch
+import torch.nn.functional as F
+from tqdm import *
+from utils.testloss import TestLoss
+from einops import rearrange
+from model_dict import get_model
+from utils.normalizer import UnitTransformer
+import matplotlib.pyplot as plt
+import time
+import wandb
+from scipy.io import loadmat
+
+parser = argparse.ArgumentParser('Training Transolver')
+
+def shuffle(x,y, seed=1):
+    np.random.seed(seed)
+    idx = np.arange(len(x))
+    np.random.shuffle(idx)
+    x = x[idx]
+    y = y[idx]
+    return x,y
+
+def set_seed(seed):    
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    torch.cuda.manual_seed(seed)
+
+parser.add_argument('--lr', type=float, default=1e-3)
+parser.add_argument('--epochs', type=int, default=500)
+parser.add_argument('--weight_decay', type=float, default=1e-5)
+parser.add_argument('--model', type=str, default='Transolver_Irregular_Mesh')
+parser.add_argument('--n-hidden', type=int, default=32, help='hidden dim')
+parser.add_argument('--n-layers', type=int, default=4, help='layers')
+parser.add_argument('--n-heads', type=int, default=4)
+parser.add_argument('--batch-size', type=int, default=20)
+parser.add_argument("--gpu", type=str, default='0', help="GPU index to use")
+parser.add_argument('--max_grad_norm', type=float, default=None)
+parser.add_argument('--downsample', type=int, default=1)
+parser.add_argument('--mlp_ratio', type=int, default=1)
+parser.add_argument('--dropout', type=float, default=0.0)
+parser.add_argument('--ntrain', type=int, default=1000)
+parser.add_argument('--unified_pos', type=int, default=0)
+parser.add_argument('--ref', type=int, default=8)
+parser.add_argument('--project-name', type=str, default='transolver_manifold_many')
+parser.add_argument('--slice-num', type=int, default=16)
+parser.add_argument('--eval', type=int, default=0)
+parser.add_argument('--seed', type=int, default=1)
+parser.add_argument('--wandb', action='store_true')
+parser.add_argument('--norm-grid', action='store_true')
+parser.add_argument('--dir', type=str, default='/projects/bgcs/mlowery/manifold_datasets')
+parser.add_argument('--npoints', default=2400) ### torus: 2400, 5046, 10086; sphere = 2562, 5762, 10242
+parser.add_argument('--val', action='store_true')
+parser.add_argument('--problem', type=str, default='rd', choices=['nlp', 'poisson', 'rd'])
+parser.add_argument('--k-train', type=int, default=2) ### max is 12
+parser.add_argument('--k-test', type=int, default=5)
+
+args = parser.parse_args()
+set_seed(args.seed)
+
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+
+ntrain = args.ntrain
+epochs = args.epochs
+
+
+def count_parameters(model):
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        if not parameter.requires_grad: continue
+        params = parameter.numel()
+        total_params += params
+    print(f"Total Trainable Params: {total_params}")
+    return total_params
+
+def main():
+    ########## load data ########################################################################
+
+    ### wants N,n,1 input for funcs
+    if not args.wandb:
+        os.environ["WANDB_MODE"] = "disabled"
+    wandb.login(key='d612cda26a5690e196d092756d668fc2aee8525b')
+    wandb.init(project=args.project_name, name=f'{args.problem}_{args.k_train}')
+    wandb.config.update(args)
+
+    data = loadmat(os.path.join(args.dir, f'frac_{args.problem}_isPositive_manifold_many.mat'))
+    F_train = np.stack([x[0] for x in data['F_train']]); F_test = np.stack([x[0] for x in data['F_test']])
+    U_train = np.stack([x[0] for x in data['U_train']]); U_test = np.stack([x[0] for x in data['U_test']])
+    x_grid = np.stack([x[0] for x in data['surfs']])
+
+    manifold_coords = data['t'][0]
+
+    key,_ = jr.split(key)
+    perm = jr.permutation(key, len(manifold_coords))
+    train_manifold_idx = perm[:args.k_train]; test_manifold_idx = perm[-args.k_test:]
+
+    F_train = F_train[train_manifold_idx]
+    mc = manifold_coords[train_manifold_idx] 
+    mc = np.broadcast_to(mc[:,None,None], F_train.shape)
+    F_train = np.concatenate((F_train[...,None], mc[...,None]), axis=-1) # k,N,n,2
+
+    F_test = F_test[test_manifold_idx] # k,N,n
+    mc = manifold_coords[test_manifold_idx] 
+    mc = np.broadcast_to(mc[:,None,None], F_test.shape)
+    F_test = np.concatenate((F_test[...,None], mc[...,None]), axis=-1) # k,N,n,2
+
+    U_train = U_train[train_manifold_idx]; U_test = U_test[test_manifold_idx]
+    U_train = U_train.reshape(-1, U_train.shape[-1])
+    U_test = U_test.reshape(-1, U_test.shape[-1])
+    ###########################################################################################################
+
+    ### don't have manifold coords unforch, crap
+    x_train, x_test = F_train, F_test ## shuffle these?
+    y_train, y_test = U_train, U_test
+
+    key,_ = jr.split(key)
+    perm = jr.permutation(key, len(x_train))
+    x_train = x_train[perm]; y_train = y_train[perm]
+    ntrain = len(x_train)
+
+    print(f'{x_train.shape=}, {x_test.shape=}, {y_train.shape=}, {y_test.shape=}')
+
+    n_samples = x_train.shape[1]
+
+    x_grid_train = np.repeat(x_grid[train_manifold_idx,None], ntrain, axis=1)
+    x_grid_train = x_grid_train.reshape(-1,x_grid.shape[1], 3)
+
+    x_grid_test = np.repeat(x_grid[test_manifold_idx,None], ntest, axis=1)
+    x_grid_test = x_grid_test.reshape(-1,x_grid.shape[1], 3)
+
+    print(f'{x_train.shape=}, {x_test.shape=}, {y_train.shape=}, {y_test.shape=}')
+    if args.norm_grid:
+        x_grid_min, x_grid_max = np.min(x_grid, axis=0, keepdims=True), np.max(x_grid, axis=0, keepdims=True)
+        x_grid = (x_grid- x_grid_min) / (x_grid_max - x_grid_min)
+
+    x_train = torch.tensor(x_train, dtype=torch.float32)
+    x_test =  torch.tensor(x_test, dtype=torch.float32)
+    y_train = torch.tensor(y_train, dtype=torch.float32)
+    y_test = torch.tensor(y_test, dtype=torch.float32)
+    x_grid_train = torch.tensor(x_grid_train, dtype=torch.float32)
+    x_grid_test = torch.tensor(x_grid_test, dtype=torch.float32)
+    ###########################
+
+    x_normalizer = UnitTransformer(x_train)
+    y_normalizer = UnitTransformer(y_train)
+
+    x_train = x_normalizer.encode(x_train)
+    x_test = x_normalizer.encode(x_test)
+    y_train = y_normalizer.encode(y_train)
+
+    x_normalizer.cuda()
+    y_normalizer.cuda()
+
+    pos_train = x_grid_train 
+    pos_test = x_grid_test 
+
+    print("Dataloading is over.")
+    train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(pos_train, x_train, y_train),
+                                               batch_size=args.batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(pos_test, x_test, y_test),
+                                              batch_size=args.batch_size, shuffle=False)
+    in_channels = x_train.shape[-1]
+    out_channels = 1
+
+    model = get_model(args).Model(space_dim=3,
+                                  n_layers=args.n_layers,
+                                  n_hidden=args.n_hidden,
+                                  dropout=args.dropout,
+                                  n_head=args.n_heads,
+                                  Time_Input=False, ### what this be
+                                  mlp_ratio=args.mlp_ratio,
+                                  fun_dim=in_channels,
+                                  out_dim=out_channels,
+                                  slice_num=args.slice_num,
+                                  ref=args.ref,
+                                  unified_pos=args.unified_pos,
+                                  ).cuda()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+
+    print(args)
+    print(model)
+    num_params = count_parameters(model)
+    wandb.log({'param_count': num_params})
+
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=args.lr, epochs=epochs,
+                                                    steps_per_epoch=len(train_loader))
+    myloss = TestLoss(size_average=False)
+
+    train_t1 = time.perf_counter()
+    for ep in range(args.epochs):
+        
+        model.train()
+        train_loss = 0
+        for x, fx, y in train_loader:
+            x, fx, y = x.cuda(), fx.cuda(), y.cuda()
+            optimizer.zero_grad()
+            out = model(x, fx=fx).squeeze(-1)  # B, N , 2, fx: B, N, y: B, N
+            out = y_normalizer.decode(out)
+            y = y_normalizer.decode(y)
+
+            l2loss = myloss(out, y)
+            loss = l2loss
+            loss.backward()
+
+            if args.max_grad_norm is not None:
+                torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
+            optimizer.step()
+            train_loss += l2loss.item()
+            scheduler.step()
+
+        train_loss /= ntrain
+        print("Epoch {} Train loss : {:.5f}".format(ep, train_loss))
+        wandb.log({'train_loss': train_loss}, step=ep)
+    train_t2 = time.perf_counter()
+       
+    rel_err = 0.0
+    eval_t1 = time.perf_counter()
+    model.eval()
+    with torch.no_grad():
+        for x, fx, y in test_loader:
+            x, fx, y = x.cuda(), fx.cuda(), y.cuda()
+            out = model(x, fx=fx).squeeze(-1)
+            out = y_normalizer.decode(out)
+            out = torch.linalg.norm(out, dim=-1)
+            y = torch.linalg.norm(y, dim=-1)
+            tl = myloss(out, y).item()
+            rel_err += tl
+    rel_err /= ntest
+    print("rel_err:{}".format(rel_err))
+    eval_t2 = time.perf_counter()
+    print('eval_time: ', eval_t2-eval_t1, 'train_time: ', train_t2-train_t1)
+    wandb.log({"test_loss": rel_err, 'eval_time:': eval_t2-eval_t1, 'train_time': train_t2 - train_t1}, step=ep, commit=True)
+
+
+if __name__ == "__main__":
+    main()
