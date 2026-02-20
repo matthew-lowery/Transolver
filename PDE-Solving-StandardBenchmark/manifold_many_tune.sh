80e25878c51b44768d984f1bfbfcec1f8b1a834b
matthew-lowery <mattlowery10@gmail.com>
2026-02-20 12:02:53 -0700

    setting up exps

--- PDE-Solving-StandardBenchmark/manifold_many_tune.sh
+++ PDE-Solving-StandardBenchmark/manifold_many_tune.sh
@@ -35,11 +35,9 @@ EOF
 #sp "python3 -u manifold.py --project-name=manifold_final --seed=$seed --n-heads=4 --norm-grid=3 --n-hidden=128 --n-layers=8 --slice-num=64 --epochs=500 --ntrain=$ntrain --problem='ADRSHEAR' --wandb" 10
 #
 
-
-for ktrain in 2 4 6 8 10 12; do
+for ktrain in 1 5 10 15 20 25 30; do
 for seed in 1 2 3; do
-sp "python3 -u manifold_many.py --seed=$seed --n-heads=8 --batch-size=20 --norm-grid=3 --n-hidden=64 --n-layers=4 --slice-num=32 --epochs=500 --k-train=$ktrain --problem='rd' --wandb" 
-sp "python3 -u manifold_many.py --seed=$seed --n-heads=4 --batch-size=20 --norm-grid=0 --n-hidden=128 --n-layers=4 --slice-num=64 --epochs=500 --k-train=$ktrain --problem='poisson' --wandb" 
+sp "python3 -u manifold_many.py --seed=$seed --n-heads=6 --batch-size=50 --norm-grid=3 --n-hidden=64 --n-layers=4 --slice-num=32 --epochs=500 --k-train=$ktrain --problem='rd' --wandb"
 done
 done
 
